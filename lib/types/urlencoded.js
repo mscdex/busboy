mscdex/busboy

commit 36e209a939f75c5f6cfeba2e148bbbd70a298b75

urlencoded: remove iconv-lite usage

lib/types/urlencoded.js
@@ -1,6 +1,5 @@
-var iconv = require('iconv-lite');
-
-var Decoder = require('../utils').Decoder;
+var jsencoding = require('../../deps/encoding/encoding'),
+    Decoder = require('../utils').Decoder;
 
 var RE_CHARSET = /^charset$/i;
 
@@ -28,12 +27,6 @@ function UrlEncoded(boy, limits, headers, parsedConType) {
 
   if (charset === undefined)
     charset = 'ISO-8859-1';
-  else if (charset === 'us-ascii')
-    charset = 'ascii';
-  else if (charset === 'utf-8')
-    charset = 'utf8';
-  else if (charset === 'utf-16le')
-    charset = 'ucs2';
 
   this.decoder = new Decoder();
   this.charset = charset;
@@ -106,9 +99,7 @@ UrlEncoded.prototype.write = function(data) {
         this.decoder.reset();
 
         if (key.length) {
-          this.boy.emit('field', iconv.decode(
-                                   new Buffer(key, 'binary'),
-                                   this.charset),
+          this.boy.emit('field', convertVal(buffer, this.charset),
                                  undefined,
                                  false,
                                  keyTrunc);
@@ -157,14 +148,8 @@ UrlEncoded.prototype.write = function(data) {
           else
             this._val += this.decoder.write(data.toString('binary', p, idxamp));
         }
-        this.boy.emit('field', iconv.decode(
-                                 new Buffer(this._key, 'binary'),
-                                 this.charset),
-                               (this._val
-                                ? iconv.decode(
-                                    new Buffer(this._val, 'binary'),
-                                    this.charset)
-                                : this._val),
+        this.boy.emit('field', convertVal(this._key, this.charset),
+                               convertVal(this._val, this.charset),
                                this._valTrunc,
                                this._keyTrunc);
         this._state = 'key';
@@ -209,25 +194,28 @@ UrlEncoded.prototype.write = function(data) {
 
 UrlEncoded.prototype.end = function() {
   if (this._state === 'key' && this._key.length > 0) {
-    this.boy.emit('field', iconv.decode(
-                             new Buffer(this._key, 'binary'),
-                             this.charset),
+    this.boy.emit('field', convertVal(this._key, this.charset),
                            undefined,
                            false,
                            this._keyTrunc);
   } else if (this._state === 'val') {
-    this.boy.emit('field', iconv.decode(
-                             new Buffer(this._key, 'binary'),
-                             this.charset),
-                           (this._val
-                            ? iconv.decode(
-                                new Buffer(this._val, 'binary'),
-                                this.charset)
-                            : this._val),
+    this.boy.emit('field', convertVal(this._key, this.charset),
+                           convertVal(this._val, this.charset),
                            this._valTrunc,
                            this._keyTrunc);
   }
   this.boy.emit('end');
 };
+
+function convertVal(val, charset) {
+  var ret = val;
+  if (val && val.length && jsencoding.encodingExists(charset)) {
+    try {
+      ret = jsencoding.TextDecoder(charset)
+                      .decode(new Buffer(val, 'binary'));
+    } catch(e) {}
+  }
+  return ret;
+}
 
 module.exports = UrlEncoded;

commit a6eb98563441e8397a28e61a88cefe5ce8a18554

urlencoded: fix bug introduced in 36e209a93

lib/types/urlencoded.js
@@ -99,7 +99,7 @@ UrlEncoded.prototype.write = function(data) {
         this.decoder.reset();
 
         if (key.length) {
-          this.boy.emit('field', convertVal(buffer, this.charset),
+          this.boy.emit('field', convertVal(key, this.charset),
                                  undefined,
                                  false,
                                  keyTrunc);