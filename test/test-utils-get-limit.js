0c0f9ceb90e1bdea4e89d1192f6e50de493d1ac4
John Lees-Miller <jdleesmiller@gmail.com>
2019-06-22 19:15:25 +0100

    Add tests for null and NaN limits

--- test/test-utils-get-limit.js
+++ test/test-utils-get-limit.js
@@ -7,10 +7,16 @@ assert.deepEqual(getLimit(undefined, 'fileSize', Infinity), Infinity);
 
 assert.deepEqual(getLimit({}, 'fieldSize', 1), 1);
 assert.deepEqual(getLimit({}, 'fileSize', Infinity), Infinity);
+assert.deepEqual(getLimit({ fieldSize: null }, 'fieldSize', 1), 1);
+assert.deepEqual(getLimit({ fileSize: null }, 'fileSize', Infinity), Infinity);
 
 assert.deepEqual(getLimit({ fieldSize: 0 }, 'fieldSize', 1), 0);
 assert.deepEqual(getLimit({ fileSize: 2 }, 'fileSize', 1), 2);
 
 assert.throws(function() {
   getLimit({ fieldSize: '1' }, 'fieldSize', 1);
+}, /^Error: Limit fieldSize is not a valid number$/);
+
+assert.throws(function() {
+  getLimit({ fieldSize: NaN }, 'fieldSize', 1);
 }, /^Error: Limit fieldSize is not a valid number$/);
