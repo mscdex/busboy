1642554981f1bc974b6c5c4e90b52a9db4c59d2a
John Lees-Miller <jdleesmiller@gmail.com>
2019-06-22 15:54:02 +0100

    Use deepEqual for older node versions

--- test/test-utils-get-limit.js
+++ test/test-utils-get-limit.js
@@ -1,15 +1,15 @@
 var getLimit = require('../lib/utils').getLimit;
 
-var assert = require('assert').strict;
+var assert = require('assert');
 
-assert.deepStrictEqual(getLimit(undefined, 'fieldSize', 1), 1);
-assert.deepStrictEqual(getLimit(undefined, 'fileSize', Infinity), Infinity);
+assert.deepEqual(getLimit(undefined, 'fieldSize', 1), 1);
+assert.deepEqual(getLimit(undefined, 'fileSize', Infinity), Infinity);
 
-assert.deepStrictEqual(getLimit({}, 'fieldSize', 1), 1);
-assert.deepStrictEqual(getLimit({}, 'fileSize', Infinity), Infinity);
+assert.deepEqual(getLimit({}, 'fieldSize', 1), 1);
+assert.deepEqual(getLimit({}, 'fileSize', Infinity), Infinity);
 
-assert.deepStrictEqual(getLimit({ fieldSize: 0 }, 'fieldSize', 1), 0);
-assert.deepStrictEqual(getLimit({ fileSize: 2 }, 'fileSize', 1), 2);
+assert.deepEqual(getLimit({ fieldSize: 0 }, 'fieldSize', 1), 0);
+assert.deepEqual(getLimit({ fileSize: 2 }, 'fileSize', 1), 2);
 
 assert.throws(function() {
   getLimit({ fieldSize: '1' }, 'fieldSize', 1);
