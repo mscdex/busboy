a2d4b94e304340ba59f85fc09e42ba7e22161ba9
John Lees-Miller <jdleesmiller@gmail.com>
2019-06-23 07:52:46 +0100

    Use strictEqual in tests

--- test/test-utils-get-limit.js
+++ test/test-utils-get-limit.js
@@ -2,16 +2,16 @@ var getLimit = require('../lib/utils').getLimit;
 
 var assert = require('assert');
 
-assert.deepEqual(getLimit(undefined, 'fieldSize', 1), 1);
-assert.deepEqual(getLimit(undefined, 'fileSize', Infinity), Infinity);
+assert.strictEqual(getLimit(undefined, 'fieldSize', 1), 1);
+assert.strictEqual(getLimit(undefined, 'fileSize', Infinity), Infinity);
 
-assert.deepEqual(getLimit({}, 'fieldSize', 1), 1);
-assert.deepEqual(getLimit({}, 'fileSize', Infinity), Infinity);
-assert.deepEqual(getLimit({ fieldSize: null }, 'fieldSize', 1), 1);
-assert.deepEqual(getLimit({ fileSize: null }, 'fileSize', Infinity), Infinity);
+assert.strictEqual(getLimit({}, 'fieldSize', 1), 1);
+assert.strictEqual(getLimit({}, 'fileSize', Infinity), Infinity);
+assert.strictEqual(getLimit({ fieldSize: null }, 'fieldSize', 1), 1);
+assert.strictEqual(getLimit({ fileSize: null }, 'fileSize', Infinity), Infinity);
 
-assert.deepEqual(getLimit({ fieldSize: 0 }, 'fieldSize', 1), 0);
-assert.deepEqual(getLimit({ fileSize: 2 }, 'fileSize', 1), 2);
+assert.strictEqual(getLimit({ fieldSize: 0 }, 'fieldSize', 1), 0);
+assert.strictEqual(getLimit({ fileSize: 2 }, 'fileSize', 1), 2);
 
 assert.throws(function() {
   getLimit({ fieldSize: '1' }, 'fieldSize', 1);
