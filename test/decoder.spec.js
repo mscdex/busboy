82169a9d96bb8772dd0395f204134348c6e152b4
Igor Savin <iselwin@gmail.com>
2021-11-27 16:19:27 +0200

    Move more tests over to Mocha (#12)

---
+++ test/decoder.spec.js
@@ -0,0 +1,81 @@
+const {assert} = require('chai');
+const {Decoder} = require('../lib/utils');
+
+describe('Decoder', () => {
+    [
+        {
+            source: ['Hello world'],
+            expected: 'Hello world',
+            what: 'No encoded bytes'
+        },
+        {
+            source: ['Hello%20world'],
+            expected: 'Hello world',
+            what: 'One full encoded byte'
+        },
+        {
+            source: ['Hello%20world%21'],
+            expected: 'Hello world!',
+            what: 'Two full encoded bytes'
+        },
+        {
+            source: ['Hello%', '20world'],
+            expected: 'Hello world',
+            what: 'One full encoded byte split #1'
+        },
+        {
+            source: ['Hello%2', '0world'],
+            expected: 'Hello world',
+            what: 'One full encoded byte split #2'
+        },
+        {
+            source: ['Hello%20', 'world'],
+            expected: 'Hello world',
+            what: 'One full encoded byte (concat)'
+        },
+        {
+            source: ['Hello%2Qworld'],
+            expected: 'Hello%2Qworld',
+            what: 'Malformed encoded byte #1'
+        },
+        {
+            source: ['Hello%world'],
+            expected: 'Hello%world',
+            what: 'Malformed encoded byte #2'
+        },
+        {
+            source: ['Hello+world'],
+            expected: 'Hello world',
+            what: 'Plus to space'
+        },
+        {
+            source: ['Hello+world%21'],
+            expected: 'Hello world!',
+            what: 'Plus and encoded byte'
+        },
+        {
+            source: ['5%2B5%3D10'],
+            expected: '5+5=10',
+            what: 'Encoded plus'
+        },
+        {
+            source: ['5+%2B+5+%3D+10'],
+            expected: '5 + 5 = 10',
+            what: 'Spaces and encoded plus'
+        },
+    ].forEach((v) => {
+        it(v.what, () => {
+
+            const dec = new Decoder();
+            let result = '';
+            v.source.forEach(function (s) {
+                result += dec.write(s);
+            });
+            const msg = 'Decoded string mismatch.\n'
+                + 'Saw: ' + result + '\n'
+                + 'Expected: ' + v.expected;
+            assert.deepEqual(result, v.expected, msg);
+        });
+    })
+})
+
---
+++ test/multipart-stream-pause.spec.js
@@ -0,0 +1,84 @@
+const { inspect } = require('util');
+const { assert } = require('chai');
+const Busboy = require('..');
+
+const BOUNDARY = 'u2KxIV5yF1y+xUspOQCCZopaVgeV6Jxihv35XQJmuTx8X3sh';
+
+function formDataSection(key, value) {
+  return Buffer.from('\r\n--' + BOUNDARY
+                     + '\r\nContent-Disposition: form-data; name="'
+                     + key + '"\r\n\r\n' + value);
+}
+function formDataFile(key, filename, contentType) {
+  return Buffer.concat([
+    Buffer.from('\r\n--' + BOUNDARY + '\r\n'),
+    Buffer.from('Content-Disposition: form-data; name="'
+                + key + '"; filename="' + filename + '"\r\n'),
+    Buffer.from('Content-Type: ' + contentType + '\r\n\r\n'),
+    Buffer.allocUnsafe(100000)
+  ]);
+}
+
+describe('multipart-stream-pause', () => {
+  it('processes stream correctly', () => {
+    const reqChunks = [
+      Buffer.concat([
+        formDataFile('file', 'file.bin', 'application/octet-stream'),
+        formDataSection('foo', 'foo value')
+      ]),
+      formDataSection('bar', 'bar value'),
+      Buffer.from('\r\n--' + BOUNDARY + '--\r\n')
+    ];
+    const busboy = new Busboy({
+      headers: {
+        'content-type': 'multipart/form-data; boundary=' + BOUNDARY
+      }
+    });
+    let finishes = 0;
+    const results = [];
+    const expected = [
+      ['file', 'file', 'file.bin', '7bit', 'application/octet-stream'],
+      ['field', 'foo', 'foo value', false, false, '7bit', 'text/plain'],
+      ['field', 'bar', 'bar value', false, false, '7bit', 'text/plain'],
+    ];
+
+    busboy.on('field', function(key, val, keyTrunc, valTrunc, encoding, contype) {
+      results.push(['field', key, val, keyTrunc, valTrunc, encoding, contype]);
+    });
+    busboy.on('file', function(fieldname, stream, filename, encoding, mimeType) {
+      results.push(['file', fieldname, filename, encoding, mimeType]);
+      // Simulate a pipe where the destination is pausing (perhaps due to waiting
+      // for file system write to finish)
+      setTimeout(function() {
+        stream.resume();
+      }, 10);
+    });
+    busboy.on('finish', function() {
+      assert(finishes++ === 0, 'finish emitted multiple times');
+      assert.deepEqual(results.length,
+          expected.length,
+          'Parsed result count mismatch. Saw '
+          + results.length
+          + '. Expected: ' + expected.length);
+
+      results.forEach(function(result, i) {
+        assert.deepEqual(result,
+            expected[i],
+            'Result mismatch:\nParsed: ' + inspect(result)
+            + '\nExpected: ' + inspect(expected[i]));
+      });
+    }).on('error', function(err) {
+      assert(false, 'Unexpected error: ' + err.stack);
+    });
+
+    reqChunks.forEach(function(buf) {
+      busboy.write(buf);
+    });
+    busboy.end();
+
+    process.on('exit', function() {
+      assert(finishes === 1, 'busboy did not finish');
+    });
+  })
+})
+
