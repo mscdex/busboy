284318edcffe45b73b9dff29fbb4c936d196458c
Uzlopak <aras.abbasi@googlemail.com>
2021-11-28 16:34:53 +0100

    increase benchmark rounds to 10 (#17)

--- deps/dicer/bench/dicer-bench-multipart-parser.js
+++ deps/dicer/bench/dicer-bench-multipart-parser.js
@@ -1,53 +1,11 @@
-var Dicer = require('../lib/Dicer'),
-    boundary = '-----------------------------168072824752491622650073',
-    d = new Dicer({ boundary: boundary }),
-    mb = 100,
-    buffer = createMultipartBuffer(boundary, mb * 1024 * 1024),
-    callbacks =
-      { partBegin: -1,
-        partEnd: -1,
-        headerField: -1,
-        headerValue: -1,
-        partData: -1,
-        end: -1,
-      };
-
-
-d.on('part', function(p) {
-  callbacks.partBegin++;
-  p.on('header', function(header) {
-    /*for (var h in header)
-      console.log('Part header: k: ' + inspect(h) + ', v: ' + inspect(header[h]));*/
-  });
-  p.on('data', function(data) {
-    callbacks.partData++;
-    //console.log('Part data: ' + inspect(data.toString()));
-  });
-  p.on('end', function() {
-    //console.log('End of part\n');
-    callbacks.partEnd++;
-  });
-});
-d.on('end', function() {
-  //console.log('End of parts');
-  callbacks.end++;
-});
-
-var start = +new Date(),
-    nparsed = d.write(buffer),
-    duration = +new Date - start,
-    mbPerSec = (mb / (duration / 1000)).toFixed(2);
-
-console.log(mbPerSec+' mb/sec');
-
-//assert.equal(nparsed, buffer.length);
+const Dicer = require('../lib/Dicer')
 
 function createMultipartBuffer(boundary, size) {
-  var head =
-        '--'+boundary+'\r\n'
-      + 'content-disposition: form-data; name="field1"\r\n'
-      + '\r\n'
-    , tail = '\r\n--'+boundary+'--\r\n'
+  const head =
+    '--' + boundary + '\r\n'
+    + 'content-disposition: form-data; name="field1"\r\n'
+    + '\r\n'
+    , tail = '\r\n--' + boundary + '--\r\n'
     , buffer = Buffer.allocUnsafe(size);
 
   buffer.write(head, 0, 'ascii');
@@ -55,8 +13,46 @@ function createMultipartBuffer(boundary, size) {
   return buffer;
 }
 
-process.on('exit', function() {
-  /*for (var k in callbacks) {
-    assert.equal(0, callbacks[k], k+' count off by '+callbacks[k]);
-  }*/
-});
+for (let i = 0, il = 10; i < il; i++) {
+  const boundary = '-----------------------------168072824752491622650073',
+    d = new Dicer({ boundary: boundary }),
+    mb = 100,
+    buffer = createMultipartBuffer(boundary, mb * 1024 * 1024),
+    callbacks =
+    {
+      partBegin: -1,
+      partEnd: -1,
+      headerField: -1,
+      headerValue: -1,
+      partData: -1,
+      end: -1,
+    };
+
+
+  d.on('part', function (p) {
+    callbacks.partBegin++;
+    p.on('header', function (header) {
+      /*for (var h in header)
+      console.log('Part header: k: ' + inspect(h) + ', v: ' + inspect(header[h]));*/
+    });
+    p.on('data', function (data) {
+      callbacks.partData++;
+      //console.log('Part data: ' + inspect(data.toString()));
+    });
+    p.on('end', function () {
+      //console.log('End of part\n');
+      callbacks.partEnd++;
+    });
+  });
+  d.on('end', function () {
+    //console.log('End of parts');
+    callbacks.end++;
+  });
+
+  const start = +new Date();
+  d.write(buffer);
+  const duration = +new Date - start;
+  const mbPerSec = (mb / (duration / 1000)).toFixed(2);
+
+  console.log(mbPerSec + ' mb/sec');
+}
